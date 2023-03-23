b82ada163276259f9b546da272d8ec9a9cf92265
DaniloDuarte <danilojoseduarte@gmail.com>
2023-03-23 15:56:19 -0300

    Validação realizada no id usernameValue

--- Js/script.js
+++ Js/script.js
@@ -3,3 +3,16 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("password-confirmation");
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    checkInputUsername();
+})
+
+function checkInputUsername (){
+    const usernameValue = username.value;
+
+    console.log(usernameValue)
+}
