1385bbb72570dfc7305e3939a252c845e1599788
DaniloDuarte <danilojoseduarte@gmail.com>
2023-03-27 16:31:46 -0300

    Validações via JavaScript

--- Js/script.js
+++ Js/script.js
@@ -4,30 +4,54 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("password-confirmation");
 
-
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    checkInputUsername();
-})
+  checkInputUsername();
+  checkInputEmail();
+  checkInputPassword();
+});
 
-function checkInputUsername (){
-    const usernameValue = username.value;
+function checkInputUsername() {
+  const usernameValue = username.value;
 
-    if (usernameValue === ""){
-        errorInput(username, "Preencha um username!")
-    } else {
-        const formItem = username.parentElement;
-        formItem.classList = "form-content"
-    }
-    
+  if (usernameValue === "") {
+    errorInput(username, "Preencha um username!");
+  } else {
+    const formItem = username.parentElement;
+    formItem.className = "form-content";
+  }
 }
 
-function errorInput (input, message) {
-    const formItem = input.parentElement;
-    const textMessage = formItem.querySelector("a")
+function checkInputEmail() {
+  const emailValue = email.value;
 
-    textMessage.innerText = message;
+  if (emailValue === "") {
+    errorInput(email, "O email é obrigatório.");
+  } else {
+    const formItem = email.parentElement;
+    formItem.className = "form-content";
+  }
+}
 
-    formItem.className = "form-content error"
+function checkInputPassword() {
+  const passwordValue = password.value;
+
+  if (passwordValue === "") {
+    errorInput(password, "A senha é obrigatória.");
+  } else if (passwordValue.length < 8) {
+    errorInput(password, "A senha precisa ter no mínimo 8 caracteres.")
+  } else {
+    const formItem = password.parentElement;
+    formItem.className = "form-content";
+  }
+}
+
+function errorInput(input, message) {
+  const formItem = input.parentElement;
+  const textMessage = formItem.querySelector("a");
+
+  textMessage.innerText = message;
+
+  formItem.className = "form-content error";
 }
